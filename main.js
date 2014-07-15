03094ae26b4f56896f0ceb659ef2d498c6fe71d4
Paul Stasiuk <pstasiuk@Pauls-MacBook-Air-2.local>
2014-07-14 20:27:43 -0500

    testing again.

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var restify = require('restify');
 var mongojs = require("mongojs");
 
+#Root User:     admin
+#Root Password: 9ramFj1Xzgm7
+#Database Name: complaints
+#mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/
  
 var ip_addr = '127.0.0.1';
 var port    =  '8080';
@@ -10,6 +14,10 @@ function basicResponse(req,res,next){
 	next();
 }
 
+function newMessageParge(req,res,next){
+	res.send('thanks, your complaint has been noted.');
+	next();
+}
 
 var server = restify.createServer({
     name : "myapp"
@@ -20,7 +28,7 @@ server.use(restify.bodyParser());
 server.use(restify.CORS());
  
 server.get('/',basicResponse);
-
+server.get('/incoming/new',newMessageParse);
 
 server.listen(port ,ip_addr, function(){
     console.log('%s listening at %s ', server.name , server.url);
